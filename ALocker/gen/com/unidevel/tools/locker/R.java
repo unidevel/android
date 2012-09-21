cb902f1f9f82f1f2c130a3c122c523f0c0af9935
unidevel <unidevel@hotmail.com>
2012-09-21 16:03:32 +0800

    a

--- ALocker/gen/com/unidevel/tools/locker/R.java
+++ ALocker/gen/com/unidevel/tools/locker/R.java
@@ -17,26 +17,19 @@ public final class R {
         public static final int shutdown=0x7f020003;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f060003;
-        public static final int btnNo=0x7f060005;
-        public static final int btnYes=0x7f060004;
         public static final int checkBoot=0x7f060000;
-        public static final int imgCancel=0x7f06000c;
-        public static final int imgLock=0x7f060007;
-        public static final int imgReboot=0x7f060001;
-        public static final int imgShutdown=0x7f06000a;
-        public static final int labelCancel=0x7f06000d;
-        public static final int labelLock=0x7f060008;
-        public static final int labelReboot=0x7f060002;
-        public static final int labelShutdown=0x7f06000b;
-        public static final int layoutLock=0x7f060006;
-        public static final int layoutShutdown=0x7f060009;
+        public static final int imgCancel=0x7f060007;
+        public static final int imgLock=0x7f060002;
+        public static final int imgShutdown=0x7f060005;
+        public static final int labelCancel=0x7f060008;
+        public static final int labelLock=0x7f060003;
+        public static final int labelShutdown=0x7f060006;
+        public static final int layoutLock=0x7f060001;
+        public static final int layoutShutdown=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int reboot=0x7f030001;
-        public static final int rebootconfirm=0x7f030002;
-        public static final int tools=0x7f030003;
+        public static final int tools=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
