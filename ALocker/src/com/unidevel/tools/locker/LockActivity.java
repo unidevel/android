51ed433e4bea9438c6acb48b54fc0af1b60efdf0
unidevel <unidevel@hotmail.com>
2012-10-17 10:19:31 +0800

    add icon

--- ALocker/src/com/unidevel/tools/locker/LockActivity.java
+++ ALocker/src/com/unidevel/tools/locker/LockActivity.java
@@ -3,6 +3,8 @@ package com.unidevel.tools.locker;
 import android.app.*;
 import android.content.*;
 import android.os.*;
+import android.preference.PreferenceManager;
+
 import com.unidevel.tools.locker.*;
 
 public class LockActivity extends Activity
@@ -10,6 +12,12 @@ public class LockActivity extends Activity
 	public void onCreate(Bundle bundle)
 	{
 		super.onCreate(bundle);
+		
+		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+		if(pref.getBoolean("unlock",true)&&MainActivity.checkUnlocker(this)){
+			MainActivity.startUnlocker(this);
+		}
+		
 		Intent i = new Intent(this, ActionActivity.class);
 		i.putExtra("action", ActionActivity.ACTION_LOCK);
 		i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_CLEAR_TASK);
