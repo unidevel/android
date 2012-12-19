7c55c564e35b8db61e1b171d84ab194147df8f75
unidevel <unidevel@hotmail.com>
2012-12-19 12:30:03 +0800

    w1

--- WebIDE/src/com/unidevel/webide/MainActivity.java
+++ WebIDE/src/com/unidevel/webide/MainActivity.java
@@ -1,36 +1,67 @@
 package com.unidevel.webide;
 
-import android.app.*;
-import android.os.*;
-import android.view.*;
-import android.widget.*;
-import android.webkit.*;
-import android.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
-        super.onCreate(savedInstanceState);
-		WebView view=new WebView(this);
-		final String base="file:///android_asset/www";
-		view.setWebViewClient(new WebViewClient(){
-			public boolean shouldOverrideUrlLoading(WebView view,String url)
-			{
-				Log.i("url",url);
-				if(url.startsWith("file:/")||url.startsWith("http:/")||url.startsWith("https:/")){
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		WebView view = new WebView(this);
+		setContentView(view);
+		view.getSettings().setJavaScriptEnabled(true);
+		view.getSettings().setAllowFileAccess(true);
+		view.getSettings().setSupportZoom(false);
+		final String base = "file:///android_asset/www/";
+		view.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				Log.i("url", url);
+				if (url.startsWith("file:/") || url.startsWith("http:/")
+						|| url.startsWith("https:/")) {
 					return false;
-				}
-				else{
-					url=base+"/"+url;
+				} else {
+					url = base + "/" + url;
 					view.loadUrl(url);
 					return true;
 				}
 			}
 		});
-		view.loadUrl("file:///android_asset/www/index.html");
-        setContentView(view);
-    }
+		view.loadDataWithBaseURL(base, getHtmlData("www/index.html", null), "text/html", null, null);
+		
+	}
+
+	public String getHtmlData(String assetPath, String encoding) {
+		InputStream in = null;
+		try {
+			in = this.getAssets().open(assetPath);
+			InputStreamReader reader ;
+			if ( encoding!=null)
+				reader = new InputStreamReader(in, encoding);
+			else
+				reader = new InputStreamReader(in);
+			StringBuffer sbuf = new StringBuffer();
+			char[] buf = new char[8192];
+			int len;
+			for (len = reader.read(buf); len > 0; len = reader.read(buf)) {
+				sbuf.append(buf, 0, len);
+			}
+			return sbuf.toString();
+		} catch (IOException e) {
+			try {
+				in.close();
+			} catch (Throwable ex) {
+			}
+		}
+		return null;
+	}
 }
