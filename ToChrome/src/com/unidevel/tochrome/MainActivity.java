d84e1f211abb5a53d41ca29cff7293114179a79a
unidevel <unidevel@hotmail.com>
2013-01-26 14:11:47 +0800

    d

--- ToChrome/src/com/unidevel/tochrome/MainActivity.java
+++ ToChrome/src/com/unidevel/tochrome/MainActivity.java
@@ -1,227 +1,257 @@
 package com.unidevel.tochrome;
 
-import android.app.*;
-import android.content.*;
-import android.content.SharedPreferences.*;
-import android.content.pm.*;
-import android.net.*;
-import android.os.*;
-import android.preference.*;
-import android.util.*;
-import android.view.*;
-import android.widget.*;
-import android.widget.AdapterView.*;
-import com.google.ads.*;
-import java.util.*;
-import android.text.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity implements OnItemClickListener
-{	
-    /** Called when the activity is first created. */
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
+public class MainActivity extends Activity implements OnItemClickListener {
+	/** Called when the activity is first created. */
 	ListView linkView;
 	LinkAdapter linkAdapter;
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
-        super.onCreate(savedInstanceState);
-		Uri uri=getIntent().getData();
-		if(uri!=null){
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Uri uri = getIntent().getData();
+		if (uri != null) {
 			addLink(this, uri.toString());
 			startChrome(uri);
 			this.finish();
 			return;
 		}
-        setContentView(R.layout.main);
-        this.linkView = (ListView) this.findViewById(R.id.listView1);
+		setContentView(R.layout.main);
+		this.linkView = (ListView) this.findViewById(R.id.listView1);
 		this.linkView.setItemsCanFocus(true);
 		this.linkView.setFocusable(true);
 		this.linkView.setFocusableInTouchMode(true);
 		this.linkView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 		this.registerForContextMenu(this.linkView);
 		this.linkView.setOnItemClickListener(this);
-		
-		//ActionBar bar=this.getActionBar();
-		//bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME,ActionBar.DISPLAY_SHOW_HOME);
-        //ToChrome: a150eec940a7ef9  
-        AdView adView = new AdView(this, AdSize.BANNER, "a150eec940a7ef9"); 
-		LinearLayout layout = (LinearLayout) findViewById(R.id.adLayout); 
+
+		// ActionBar bar=this.getActionBar();
+		// bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME,ActionBar.DISPLAY_SHOW_HOME);
+		// ToChrome: a150eec940a7ef9
+		AdView adView = new AdView(this, AdSize.BANNER, "a150eec940a7ef9");
+		LinearLayout layout = (LinearLayout) findViewById(R.id.adLayout);
 		layout.addView(adView);
 		AdRequest req = new AdRequest();
 		adView.loadAd(req);
-    }
+	}
 
-	static final int ACT_DELETE=100;
-	static final int ACT_VIEW=101;
-	static final int ACT_CLEAR_DEFAULT=102;
-	static final int ACT_VIEW_IN_CHROME=103;
-	static final int ACT_VIEW_SOURCE=104;
-	
-	
-	public boolean onCreateOptiondMenu (Menu menu)
-	{
+	static final int ACT_DELETE = 100;
+	static final int ACT_VIEW = 101;
+	static final int ACT_CLEAR_DEFAULT = 102;
+	static final int ACT_VIEW_IN_CHROME = 103;
+	static final int ACT_VIEW_SOURCE = 104;
+
+	public boolean onCreateOptiondMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-		MenuItem item=menu.add(Menu.NONE,ACT_DELETE,Menu.NONE,R.string.settings);
+		MenuItem item = menu.add(Menu.NONE, ACT_DELETE, Menu.NONE,
+				R.string.settings);
 		item.setIcon(android.R.drawable.ic_menu_preferences);
-		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+				| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		return true;
 	}
-	
-	public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo info)
-	{
-		super.onCreateContextMenu(menu,v,info);
-		MenuItem item=menu.add(Menu.NONE,ACT_DELETE,Menu.NONE,R.string.delete);
+
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenu.ContextMenuInfo info) {
+		super.onCreateContextMenu(menu, v, info);
+		MenuItem item = menu.add(Menu.NONE, ACT_DELETE, Menu.NONE,
+				R.string.delete);
 		item.setIcon(android.R.drawable.ic_menu_delete);
-		//item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		// item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 
-		item=menu.add(Menu.NONE,ACT_VIEW,Menu.NONE,R.string.view);
+		item = menu.add(Menu.NONE, ACT_VIEW, Menu.NONE, R.string.view);
 		item.setIcon(android.R.drawable.ic_menu_directions);
-		//item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		// item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 
-		item=menu.add(Menu.NONE,ACT_CLEAR_DEFAULT,Menu.NONE,R.string.clear_default);
+		item = menu.add(Menu.NONE, ACT_CLEAR_DEFAULT, Menu.NONE,
+				R.string.clear_default);
 		item.setIcon(android.R.drawable.ic_menu_manage);
-		//item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-		item=menu.add(Menu.NONE,ACT_VIEW_IN_CHROME,Menu.NONE,R.string.app_name);
-		item=menu.add(Menu.NONE,ACT_VIEW_SOURCE,Menu.NONE,R.string.vs_name);
+		// item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		item = menu.add(Menu.NONE, ACT_VIEW_IN_CHROME, Menu.NONE,
+				R.string.app_name);
+		item = menu
+				.add(Menu.NONE, ACT_VIEW_SOURCE, Menu.NONE, R.string.vs_name);
 	}
-	
-	public boolean onContextItemSelected(MenuItem item)
-	{
-		AdapterContextMenuInfo info=(AdapterContextMenuInfo)item.getMenuInfo();
-		TextView text= (TextView)info.targetView;
-		String link=text.getText().toString();
-		if(item.getItemId()==ACT_DELETE){
+
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+		TextView text = (TextView) info.targetView;
+		String link = text.getText().toString();
+		if (item.getItemId() == ACT_DELETE) {
 			deleteLink(link);
-		}
-		else if(item.getItemId()==ACT_VIEW){
+		} else if (item.getItemId() == ACT_VIEW) {
 			viewLink(link);
-		}
-		else if(item.getItemId()==ACT_VIEW_IN_CHROME){
-			Uri uri=Uri.parse(link);
+		} else if (item.getItemId() == ACT_VIEW_IN_CHROME) {
+			Uri uri = Uri.parse(link);
 			startChrome(uri);
-		}
-		else if(item.getItemId()==ACT_VIEW_SOURCE){
-			Uri uri=Uri.parse("view-source:"+link);
+		} else if (item.getItemId() == ACT_VIEW_SOURCE) {
+			Uri uri = Uri.parse("view-source:" + link);
 			startChrome(uri);
-		}
-		else if(item.getItemId()==ACT_CLEAR_DEFAULT){
+		} else if (item.getItemId() == ACT_CLEAR_DEFAULT) {
 			clearDefault(link);
-		}
-		else
+		} else
 			return super.onContextItemSelected(item);
 		return true;
 	}
 
-	private void deleteLink(String link)
-	{
+	private void deleteLink(String link) {
 		this.linkAdapter.deleteLink(link);
-		List<String> links=this.linkAdapter.getLinks();
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		List<String> links = this.linkAdapter.getLinks();
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
 		Editor edit = prefs.edit();
 		int i;
-		for (i = 1; i <= links.size() && i <= 25; ++ i)
-		{
+		for (i = 1; i <= links.size() && i <= 25; ++i) {
 			Log.i("tochrome", "link" + i + ":" + links.get(i - 1));
 			edit.putString("link" + i, links.get(i - 1));
 		}
-		for (; i <= 25; ++ i)
-		{
-			edit.putString("link"+i,null);
+		for (; i <= 25; ++i) {
+			edit.putString("link" + i, null);
 		}
 		edit.commit();
 	}
 
-	private void viewLink(String link)
-	{
-		if ( link == null ) {
+	private void viewLink(String link) {
+		if (link == null) {
 			return;
-		}		
-		Intent i=new Intent(Intent.ACTION_VIEW);
-		i.setData(Uri.parse(link));
-		startActivity(i);
-	}
-	
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	List<String> links = new ArrayList<String>();
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		for ( int i = 1; i <= 25; ++ i )
+		}
+		Uri data = null;
+		data = Uri.parse(link);
+		String type = MimeTypes.getType(link);
+		if (type == null) {
+			type = "text/plain";
+		}
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.addCategory(Intent.CATEGORY_DEFAULT);
+		i.setDataAndType(data, type);
+		try 
 		{
-			String link = prefs.getString("link"+i, null);
-			if ( link!=null && link.trim().length()>0 && !links.contains(link))
+			startActivity(i);
+		}
+		catch(Throwable ex)
+		{
+			Log.e("tochrome", "viewLink:"+ex.getMessage(), ex);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		List<String> links = new ArrayList<String>();
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		for (int i = 1; i <= 25; ++i) {
+			String link = prefs.getString("link" + i, null);
+			if (link != null && link.trim().length() > 0
+					&& !links.contains(link))
 				links.add(link);
 		}
-		this.linkAdapter = new LinkAdapter(this,links);
-    	this.linkView.setAdapter(this.linkAdapter);//(new ArrayAdapter<String>(this, R.layout.item, links));
-    }
-    
-	public void startChrome(Uri uri){
+		this.linkAdapter = new LinkAdapter(this, links);
+		this.linkView.setAdapter(this.linkAdapter);// (new
+													// ArrayAdapter<String>(this,
+													// R.layout.item, links));
+	}
+
+	public void startChrome(Uri uri) {
 		Intent it = new Intent(Intent.ACTION_VIEW);
 		it.setData(uri);
 		it.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		it.setClassName("com.android.chrome","com.google.android.apps.chrome.Main");
-		try{
+		it.setClassName("com.android.chrome",
+				"com.google.android.apps.chrome.Main");
+		try {
 			startActivity(it);
-		}
-		catch(ActivityNotFoundException ex){
-			try
-			{
-				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?id=com.android.chrome"));
+		} catch (ActivityNotFoundException ex) {
+			try {
+				Intent intent = new Intent(Intent.ACTION_VIEW,
+						Uri.parse("market://search?id=com.android.chrome"));
 				startActivity(intent);
-			}
-			catch(ActivityNotFoundException e){
-				Toast.makeText(this,R.string.install_chrome,3).show();
+			} catch (ActivityNotFoundException e) {
+				Toast.makeText(this, R.string.install_chrome, Toast.LENGTH_LONG)
+						.show();
 			}
 		}
 	}
 
-	public static void addLink(Context context, String newLink)
-	{
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+	public static void addLink(Context context, String newLink) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
 		List<String> links = new ArrayList<String>();
 		links.add(newLink);
-		for ( int i = 1; i <= 25; ++ i )
-		{
-			String link = prefs.getString("link"+i, null);
-			if ( link!=null && link.trim().length()>0 && !links.contains(link))
+		for (int i = 1; i <= 25; ++i) {
+			String link = prefs.getString("link" + i, null);
+			if (link != null && link.trim().length() > 0
+					&& !links.contains(link))
 				links.add(link);
 		}
 		Editor edit = prefs.edit();
-		for ( int i = 1; i <= links.size() && i<=25; ++ i )
-		{
-			edit.putString("link"+i, links.get(i-1));
+		for (int i = 1; i <= links.size() && i <= 25; ++i) {
+			edit.putString("link" + i, links.get(i - 1));
 		}
 		edit.commit();
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-		TextView text=(TextView)view;
+	public void onItemClick(AdapterView<?> adapterView, View view,
+			int position, long id) {
+		TextView text = (TextView) view;
 		String link = text.getText().toString();
 		viewLink(link);
 	}
-		
-	
-	public void clearDefault(String link){
-		if ( link == null || link.isEmpty()) return;
+
+	public void clearDefault(String link) {
+		if (link == null || link.isEmpty())
+			return;
 		Intent i = (new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
 		PackageManager pm = getPackageManager();
-		ComponentName name= i.resolveActivity(pm);
-		final ResolveInfo mInfo = pm.resolveActivity(i, PackageManager.MATCH_DEFAULT_ONLY);
-		//pm.getApplicationLabel(mInfo.activityInfo.applicationInfo), Toast.LENGTH_LONG).show();
-		String pkg=name.getPackageName();
-		if(pkg!=null&&!"android".equals(pkg))
-		{
+		ComponentName name = i.resolveActivity(pm);
+		// final ResolveInfo mInfo = pm.resolveActivity(i,
+		// PackageManager.MATCH_DEFAULT_ONLY);
+		// pm.getApplicationLabel(mInfo.activityInfo.applicationInfo),
+		// Toast.LENGTH_LONG).show();
+		String pkg = name.getPackageName();
+		if (pkg != null && !"android".equals(pkg)) {
 			Intent intent = new Intent();
 			intent.setAction(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
 			Uri uri = Uri.fromParts("package", pkg, null);
 			intent.setData(uri);
 			startActivity(intent);
-		}
-		else
-		{
-			String msg=String.format(getString(R.string.no_default),i.getData().getScheme());
-			Toast.makeText(this,msg,3).show();
+		} else {
+			String msg = String.format(getString(R.string.no_default), i
+					.getData().getScheme());
+			Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 		}
 	}
 }
