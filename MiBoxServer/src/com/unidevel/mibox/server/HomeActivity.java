6719042ebcabcea28904e4da1d933f47efa8e2bc
unidevel <unidevel@hotmail.com>
2013-05-08 13:55:57 +0800

    cc

--- MiBoxServer/src/com/unidevel/mibox/server/HomeActivity.java
+++ MiBoxServer/src/com/unidevel/mibox/server/HomeActivity.java
@@ -1,28 +1,9 @@
 
 package com.unidevel.mibox.server;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.util.ArrayList;
-import java.util.List;
-import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceEvent;
-import javax.jmdns.ServiceListener;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.wifi.WifiManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
