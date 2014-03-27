71ac64b615ccfd2fa75f052d00f062b2ffa3cfb0
unidevel <unidevel@hotmail.com>
2014-03-27 21:43:57 +0800

    c

--- WhereAreYou/src/com/unidevel/whereareyou/MapActivity.java
+++ WhereAreYou/src/com/unidevel/whereareyou/MapActivity.java
@@ -2,13 +2,20 @@ package com.unidevel.whereareyou;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class MapActivity extends Activity
 {
+	private GoogleMap mMap;
+
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.map);
+		this.mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		this.mMap.getUiSettings().setCompassEnabled( true );
+		this.mMap.getUiSettings().setMyLocationButtonEnabled( true );
 	}
 }
