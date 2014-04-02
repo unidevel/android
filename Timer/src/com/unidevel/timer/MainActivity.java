9648d4ac82092aefaf2fb3cafe54f1b0868fa6fd
unidevel <unidevel@hotmail.com>
2014-04-02 22:53:44 +0800

    vv

--- Timer/src/com/unidevel/timer/MainActivity.java
+++ Timer/src/com/unidevel/timer/MainActivity.java
@@ -4,16 +4,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.*;
-import android.media.*;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnSeekBarChangeListener
 {
@@ -27,12 +30,18 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 	SeekBar sbAlarmEnd;
 	SeekBar sbAlarmWarn;
 	TextView messageView;
+	View cardView;
 	ImageButton btnStart;
 	ImageButton btnReset;
 	
-	long alarmTime;
-	long alarmEndTime;
-	long alarmWarnTime;
+	int alarmTime;
+	int alarmEndTime;
+	int alarmWarnTime;
+	
+	boolean alarmPlayed;
+	boolean alarmEndPlayed;
+	boolean alarmWarnPlayed;
+	boolean blink;
 	
 	long time;
 	long lastTime;
@@ -55,13 +64,29 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 		this.btnReset = (ImageButton)this.findViewById( R.id.reset );
 		this.handler = new Handler();
 		this.messageView = (TextView)this.findViewById(R.id.time);
+		this.cardView = this.findViewById( R.id.card );
+		this.time = 0;
+		this.alarmEndPlayed = false;
+		this.alarmPlayed = false;
+		this.alarmWarnPlayed = false;
     }
 	
+    @Override
+    protected void onResume()
+    {
+    	super.onResume();
+    	SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences( this );
+    	this.alarmUri = pref.getString( "alarm", null );
+    	this.alarmEndUri = pref.getString( "alarm_end", null );
+    	this.alarmWarnUri = pref.getString( "alarm_warn", null );
+    }
+    
     private void updateUI()
     {
-   		this.sbAlarm.setEnabled( !this.started );
-   		this.sbAlarmEnd.setEnabled( !this.started );
-   		this.sbAlarmWarn.setEnabled( !this.started );
+   		//this.sbAlarm.setEnabled( !this.started );
+   		//this.sbAlarmEnd.setEnabled( !this.started );
+   		//this.sbAlarmWarn.setEnabled( !this.started );
+   		this.btnReset.setEnabled( !this.started );
    		if ( this.started )
    		{
    			this.btnStart.setImageResource( android.R.drawable.ic_media_pause );
@@ -70,8 +95,9 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
    		{
    			this.btnStart.setImageResource( android.R.drawable.ic_media_play );
    		}
+   		this.cardView.setBackgroundColor( Color.BLACK );
    	}
-    
+        
     private void updateTime()
     {
     	handler.post( new Runnable(){
@@ -79,8 +105,46 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
     		public void run()
     		{
     			 int seconds =(int) (time/1000.0);
-				 String message=""+seconds;
-				 messageView.setText(message);
+    			 int left = alarmEndTime - seconds;
+    			 int color = Color.BLACK;
+    			 
+    			 if ( seconds >= alarmEndTime )
+    			 {
+    				 if ( !alarmEndPlayed )
+    				 {
+    					 play( alarmEndUri );
+    					 alarmEndPlayed = true;
+    				 }
+        			 blink = !blink;
+					 color = Color.RED;
+    			 }
+    			 else if ( seconds >= alarmWarnTime )
+    			 {
+    				 if ( !alarmWarnPlayed )
+    				 {
+    					 play( alarmWarnUri );
+    					 alarmWarnPlayed = true;
+    				 }
+        			 blink = !blink;
+					 color = Color.YELLOW;
+    			 }
+    			 else if ( seconds >= alarmTime )
+    			 {
+    				 if ( !alarmPlayed )
+    				 {
+    					 play( alarmUri );
+    					 alarmPlayed = true;
+    				 }
+					 color = Color.GREEN;
+					 blink = true;
+    			 }
+    			 else
+    			 {
+    				 blink = true;
+    			 }
+    			 String message="Total: " + alarmEndTime+" seconds\nWarning: "+alarmTime+" seconds\nLast warning: "+alarmWarnTime+" seconds\nTime: "+seconds+" seconds\nLeft: "+left+" seconds\n";
+    			 messageView.setText(message);
+				 cardView.setBackgroundColor( blink? color: Color.BLACK );
     		}
     	});
     }
@@ -101,7 +165,7 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 		{
 			this.lastTime = System.currentTimeMillis();
 			this.timer = new Timer();
-			this.timer.schedule( task, 0, 100 );
+			this.timer.schedule( task, 0, 500 );
 		}
 		else
 		{
@@ -113,6 +177,11 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 	
 	public void onReset(View view){
 		this.time = 0;
+		this.alarmEndPlayed = false;
+		this.alarmPlayed = false;
+		this.alarmWarnPlayed = false;
+		updateUI();
+		updateTimeDisplay();
 	}
 	
 	public void onChooseAlarm(View view){
@@ -132,42 +201,74 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 			if (uri != null)
 			{
 				//this.chosenRingtone = uri.toString();
+				String key = null; 
 				switch(requestCode)
 				{
 					case R.id.alarm:
 						this.alarmUri = uri.toString();
+						key =  "alarm";
 						break;
 					case R.id.alarmEnd:
 						this.alarmEndUri = uri.toString();
+						key = "alarm_end";
 						break;
 					case R.id.alarmWarn:
 						this.alarmWarnUri = uri.toString();
+						key = "alarm_warn";
 						break;
+				}
+				if ( key != null )
+				{
+					SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences( this );
+					pref.edit().putString( key, uri.toString() ).commit();
 				}
 			}
 		}            
+	}
+	
+	private void adjustSeekBar(SeekBar seekBar, int max, int delta)
+	{
+		seekBar.setMax( max );
+		//if ( seekBar.getProgress() > max )
+		{
+			int newValue = max - delta;
+			if (  newValue > 0 )
+			{
+				seekBar.setProgress( newValue );
+			}
+		}
 	}
 
 	@Override
 	public void onProgressChanged( SeekBar seekBar, int progress, boolean fromUser )
 	{
-		if ( fromUser )
+		if ( true )
 		{
 			if ( seekBar == this.sbAlarm )
 			{
-				this.alarmTime = seekBar.getProgress();
+				this.alarmTime = seekBar.getProgress()*30;
 			}
 			if ( seekBar == this.sbAlarmEnd )
 			{
-				this.alarmTime = seekBar.getProgress();
+				this.alarmEndTime = seekBar.getProgress()*30;
+				adjustSeekBar(this.sbAlarmWarn, seekBar.getProgress(), 1);
+				adjustSeekBar(this.sbAlarm, seekBar.getProgress(), 2);
 			}
 			if ( seekBar == this.sbAlarmWarn )
 			{
-				this.alarmTime = seekBar.getProgress();
+				this.alarmWarnTime = seekBar.getProgress()*30;
+				adjustSeekBar(this.sbAlarm, seekBar.getProgress(), 1);
 			}
 		}
+		updateTimeDisplay();
 	}
 	
+	private void updateTimeDisplay()
+	{
+		 String message="Total: " + alarmEndTime+" seconds\nWarning: "+alarmTime+" seconds\nLast warning: "+alarmWarnTime+" seconds\n";
+		 messageView.setText(message);
+	}
+
 	public void play(String uri){
 		try {
 			Uri n;
