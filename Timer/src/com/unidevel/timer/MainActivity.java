d6bcb76000661b6628023ce3143a819ad61396af
unidevel <unidevel@hotmail.com>
2014-04-03 09:01:54 +0800

    dsd

--- Timer/src/com/unidevel/timer/MainActivity.java
+++ Timer/src/com/unidevel/timer/MainActivity.java
@@ -1,9 +1,13 @@
+
 package com.unidevel.timer;
 
 import java.util.Timer;
 import java.util.TimerTask;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.Ringtone;
@@ -12,6 +16,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -25,35 +31,37 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 	String alarmUri;
 	String alarmWarnUri;
 	String alarmEndUri;
-	
-	SeekBar sbAlarm ;
+
+	SeekBar sbAlarm;
 	SeekBar sbAlarmEnd;
 	SeekBar sbAlarmWarn;
 	TextView messageView;
 	View cardView;
 	ImageButton btnStart;
 	ImageButton btnReset;
-	
+
 	int alarmTime;
 	int alarmEndTime;
 	int alarmWarnTime;
-	
+
 	boolean alarmPlayed;
 	boolean alarmEndPlayed;
 	boolean alarmWarnPlayed;
 	boolean blink;
-	
+
 	long time;
 	long lastTime;
-	
+
 	Timer timer;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
+	MediaButtonBroadcastReceiver receiver;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate( Bundle savedInstanceState )
 	{
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		this.started=false;
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.main );
+		this.started = false;
 		this.sbAlarm = (SeekBar)this.findViewById( R.id.alarmBar );
 		this.sbAlarmEnd = (SeekBar)this.findViewById( R.id.alarmEndBar );
 		this.sbAlarmWarn = (SeekBar)this.findViewById( R.id.alarmWarnBar );
@@ -63,116 +71,155 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 		this.btnStart = (ImageButton)this.findViewById( R.id.start );
 		this.btnReset = (ImageButton)this.findViewById( R.id.reset );
 		this.handler = new Handler();
-		this.messageView = (TextView)this.findViewById(R.id.time);
+		this.messageView = (TextView)this.findViewById( R.id.time );
 		this.cardView = this.findViewById( R.id.card );
 		this.time = 0;
 		this.alarmEndPlayed = false;
 		this.alarmPlayed = false;
 		this.alarmWarnPlayed = false;
-    }
-	
-    @Override
-    protected void onResume()
-    {
-    	super.onResume();
-    	SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences( this );
-    	this.alarmUri = pref.getString( "alarm", null );
-    	this.alarmEndUri = pref.getString( "alarm_end", null );
-    	this.alarmWarnUri = pref.getString( "alarm_warn", null );
-    	updateTimeDisplay();
-    }
-    
-    private void updateUI()
-    {
-   		//this.sbAlarm.setEnabled( !this.started );
-   		//this.sbAlarmEnd.setEnabled( !this.started );
-   		//this.sbAlarmWarn.setEnabled( !this.started );
-   		this.btnReset.setEnabled( !this.started );
-   		if ( this.started )
-   		{
-   			this.btnStart.setImageResource( android.R.drawable.ic_media_pause );
-   		}
-   		else
-   		{
-   			this.btnStart.setImageResource( android.R.drawable.ic_media_play );
-   		}
-   		this.cardView.setBackgroundColor( Color.BLACK );
-   	}
-    
-    private void updateTime()
-    {
-    	handler.post( new Runnable(){
-    		@Override
-    		public void run()
-    		{
-    			 int color = Color.BLACK;
-    			 int seconds =(int) (time/1000.0);
 
-    			 if ( seconds >= alarmEndTime )
-    			 {
-    				 if ( !alarmEndPlayed )
-    				 {
-    					 play( alarmEndUri );
-    					 alarmEndPlayed = true;
-    				 }
-        			 blink = !blink;
-					 color = Color.RED;
-    			 }
-    			 else if ( seconds >= alarmWarnTime )
-    			 {
-    				 if ( !alarmWarnPlayed )
-    				 {
-    					 play( alarmWarnUri );
-    					 alarmWarnPlayed = true;
-    				 }
-        			 blink = !blink;
-					 //color = Color.YELLOW;
-        			 int delta = seconds - alarmWarnTime;
-        			 int totalDelta = alarmEndTime - alarmWarnTime;
-        			 if ( totalDelta == 0  || delta == 0)
-        			 {
-        				 color = Color.YELLOW;
-        			 }
-        			 else
-        			 {
-        				 color = 0xFFFF0000;
-        				 int g = 255 - (int)((float)255 * (float)delta / (float)totalDelta);
-        				 color |= g << 8;
-        			 }
-    			 }
-    			 else if ( seconds >= alarmTime )
-    			 {
-    				 if ( !alarmPlayed )
-    				 {
-    					 play( alarmUri );
-    					 alarmPlayed = true;
-    				 }
-					 blink = true;
-					 color = Color.GREEN;
-        			 int delta = seconds - alarmTime;
-        			 int totalDelta = alarmWarnTime - alarmTime;
-        			 if ( totalDelta == 0  || delta == 0)
-        			 {
-        				 color = Color.GREEN;
-        			 }
-        			 else
-        			 {
-        				 color = 0xFF00FF00;
-        				 int g = (int)((float)255 * (float)delta / (float)totalDelta);
-        				 color |= g << 16;
-        			 }
-    			 }
-    			 else
-    			 {
-    				 blink = true;
-    			 }
-    			 updateTimeDisplay();
-				 cardView.setBackgroundColor( blink? color: Color.BLACK );
-    		}
-    	});
-    }
-    
-	public void onStartStop(View view){
+		/* IntentFilter mediaButtonIntentFilter = new IntentFilter( Intent.ACTION_MEDIA_BUTTON );
+		receiver = new MediaButtonBroadcastReceiver();
+		this.registerReceiver( receiver, mediaButtonIntentFilter ); */
+	}
+
+	public class MediaButtonBroadcastReceiver extends BroadcastReceiver
+	{
+
+		@Override
+		public void onReceive( Context ctx, Intent intent )
+		{
+			KeyEvent event = (KeyEvent)intent.getParcelableExtra( Intent.EXTRA_KEY_EVENT );
+			/* if ( event != null)
+			{
+				Log.i("KeyEvent", "keycode = "+event.getKeyCode());
+
+				 if (event.getKeyCode() == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)
+				 {
+					 Log.i("KeyEvent", "Play");
+				 }
+				 else if (event.getKeyCode() == KeyEvent.KEYCODE_MEDIA_NEXT) 
+				 {
+					 Log.i("KeyEvent", "Next");
+				 }
+			} */ 
+		}
+
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		//this.unregisterReceiver( receiver );
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences( this );
+		this.alarmUri = pref.getString( "alarm", null );
+		this.alarmEndUri = pref.getString( "alarm_end", null );
+		this.alarmWarnUri = pref.getString( "alarm_warn", null );
+		updateTimeDisplay();
+	}
+
+	private void updateUI()
+	{
+		// this.sbAlarm.setEnabled( !this.started );
+		// this.sbAlarmEnd.setEnabled( !this.started );
+		// this.sbAlarmWarn.setEnabled( !this.started );
+		this.btnReset.setEnabled( !this.started );
+		if ( this.started )
+		{
+			this.btnStart.setImageResource( android.R.drawable.ic_media_pause );
+		}
+		else
+		{
+			this.btnStart.setImageResource( android.R.drawable.ic_media_play );
+		}
+		this.cardView.setBackgroundColor( Color.BLACK );
+	}
+
+	private void updateTime()
+	{
+		handler.post( new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				int color = Color.BLACK;
+				int seconds = (int)(time / 1000.0);
+
+				if ( seconds >= alarmEndTime )
+				{
+					if ( !alarmEndPlayed )
+					{
+						play( alarmEndUri );
+						alarmEndPlayed = true;
+					}
+					blink = !blink;
+					color = Color.RED;
+				}
+				else if ( seconds >= alarmWarnTime )
+				{
+					if ( !alarmWarnPlayed )
+					{
+						play( alarmWarnUri );
+						alarmWarnPlayed = true;
+					}
+					blink = !blink;
+					// color = Color.YELLOW;
+					int delta = seconds - alarmWarnTime;
+					int totalDelta = alarmEndTime - alarmWarnTime;
+					if ( totalDelta == 0 || delta == 0 )
+					{
+						color = Color.YELLOW;
+					}
+					else
+					{
+						color = 0xFFFF0000;
+						int g = 255 - (int)((float)255 * (float)delta / (float)totalDelta);
+						color |= g << 8;
+					}
+				}
+				else if ( seconds >= alarmTime )
+				{
+					if ( !alarmPlayed )
+					{
+						play( alarmUri );
+						alarmPlayed = true;
+					}
+					blink = true;
+					color = Color.GREEN;
+					int delta = seconds - alarmTime;
+					int totalDelta = alarmWarnTime - alarmTime;
+					if ( totalDelta == 0 || delta == 0 )
+					{
+						color = Color.GREEN;
+					}
+					else
+					{
+						color = 0xFF00FF00;
+						int g = (int)((float)255 * (float)delta / (float)totalDelta);
+						color |= g << 16;
+					}
+				}
+				else
+				{
+					blink = true;
+				}
+				updateTimeDisplay();
+				cardView.setBackgroundColor( blink
+						? color
+						: Color.BLACK );
+			}
+		} );
+	}
+
+	public void onStartStop( View view )
+	{
 		this.started = !this.started;
 		TimerTask task = new TimerTask()
 		{
@@ -197,8 +244,9 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 		}
 		updateUI();
 	}
-	
-	public void onReset(View view){
+
+	public void onReset( View view )
+	{
 		this.time = 0;
 		this.alarmEndPlayed = false;
 		this.alarmPlayed = false;
@@ -206,30 +254,31 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 		updateUI();
 		updateTimeDisplay();
 	}
-	
-	public void onChooseAlarm(View view){
-		Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-		intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
-		intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
-		intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (Uri) null);
-		this.startActivityForResult(intent, view.getId());
-	}
-	
-	protected void onActivityResult(final int requestCode, final int resultCode, final Intent intent)
-	{
-		if (resultCode == Activity.RESULT_OK)
-		{
-			Uri uri = intent.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
 
-			if (uri != null)
+	public void onChooseAlarm( View view )
+	{
+		Intent intent = new Intent( RingtoneManager.ACTION_RINGTONE_PICKER );
+		intent.putExtra( RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION );
+		intent.putExtra( RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone" );
+		intent.putExtra( RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (Uri)null );
+		this.startActivityForResult( intent, view.getId() );
+	}
+
+	protected void onActivityResult( final int requestCode, final int resultCode, final Intent intent )
+	{
+		if ( resultCode == Activity.RESULT_OK )
+		{
+			Uri uri = intent.getParcelableExtra( RingtoneManager.EXTRA_RINGTONE_PICKED_URI );
+
+			if ( uri != null )
 			{
-				//this.chosenRingtone = uri.toString();
-				String key = null; 
-				switch(requestCode)
+				// this.chosenRingtone = uri.toString();
+				String key = null;
+				switch (requestCode)
 				{
 					case R.id.alarm:
 						this.alarmUri = uri.toString();
-						key =  "alarm";
+						key = "alarm";
 						break;
 					case R.id.alarmEnd:
 						this.alarmEndUri = uri.toString();
@@ -246,16 +295,16 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 					pref.edit().putString( key, uri.toString() ).commit();
 				}
 			}
-		}            
+		}
 	}
-	
-	private void adjustSeekBar(SeekBar seekBar, int max, int delta)
+
+	private void adjustSeekBar( SeekBar seekBar, int max, int delta )
 	{
 		seekBar.setMax( max );
-		//if ( seekBar.getProgress() > max )
+		// if ( seekBar.getProgress() > max )
 		{
 			int newValue = max - delta;
-			if (  newValue > 0 )
+			if ( newValue > 0 )
 			{
 				seekBar.setProgress( newValue );
 			}
@@ -269,55 +318,67 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 		{
 			if ( seekBar == this.sbAlarm )
 			{
-				this.alarmTime = seekBar.getProgress()*30;
+				this.alarmTime = seekBar.getProgress() * 30;
 			}
 			if ( seekBar == this.sbAlarmEnd )
 			{
-				this.alarmEndTime = seekBar.getProgress()*30;
-				adjustSeekBar(this.sbAlarmWarn, seekBar.getProgress(), 1);
-				adjustSeekBar(this.sbAlarm, seekBar.getProgress(), 2);
+				this.alarmEndTime = seekBar.getProgress() * 30;
+				adjustSeekBar( this.sbAlarmWarn, seekBar.getProgress(), 1 );
+				adjustSeekBar( this.sbAlarm, seekBar.getProgress(), 2 );
 			}
 			if ( seekBar == this.sbAlarmWarn )
 			{
-				this.alarmWarnTime = seekBar.getProgress()*30;
-				adjustSeekBar(this.sbAlarm, seekBar.getProgress(), 1);
+				this.alarmWarnTime = seekBar.getProgress() * 30;
+				adjustSeekBar( this.sbAlarm, seekBar.getProgress(), 1 );
 			}
 		}
 		updateTimeDisplay();
 	}
-	
-	private String formatTime(int seconds)
+
+	private String formatTime( int seconds )
 	{
 		if ( seconds < 0 )
 		{
-			seconds = - seconds;
+			seconds = -seconds;
 		}
-		int m = seconds/60;
-		int s = seconds%60;
-		return ""+(m<10?"0"+m:m)+":"+(s<10?"0"+s:s);
-	}
-	
-	private void updateTimeDisplay()
-	{
-		 int seconds =(int) (time/1000.0);
-		 int left = alarmEndTime - seconds;
-		 String message="Total: " + formatTime(alarmEndTime)+"\nFirst: "+formatTime(alarmTime)+"\nLast : "+formatTime(alarmWarnTime)+"\n";
-		 message +="------------\nTime : "+formatTime(seconds)+"\nLeft : "+formatTime(left)+"\n";
-		 messageView.setText(message);
+		int m = seconds / 60;
+		int s = seconds % 60;
+		return "" + (m < 10
+				? "0" + m
+				: m) + ":" + (s < 10
+				? "0" + s
+				: s);
 	}
 
-	public void play(String uri){
-		try {
+	private void updateTimeDisplay()
+	{
+		int seconds = (int)(time / 1000.0);
+		int left = alarmEndTime - seconds;
+		String message =
+				"Total: " + formatTime( alarmEndTime ) + "\nFirst: " + formatTime( alarmTime ) + "\nLast : "
+						+ formatTime( alarmWarnTime ) + "\n";
+		message += "------------\nTime : " + formatTime( seconds ) + "\nLeft : " + formatTime( left ) + "\n";
+		messageView.setText( message );
+	}
+
+	public void play( String uri )
+	{
+		try
+		{
 			Uri n;
-			if(uri==null){
-				n = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			if ( uri == null )
+			{
+				n = RingtoneManager.getDefaultUri( RingtoneManager.TYPE_NOTIFICATION );
 			}
-			else{
-				n=Uri.parse(uri);
+			else
+			{
+				n = Uri.parse( uri );
 			}
-			Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), n);
+			Ringtone r = RingtoneManager.getRingtone( getApplicationContext(), n );
 			r.play();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
@@ -330,5 +391,27 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener
 	@Override
 	public void onStopTrackingTouch( SeekBar seekBar )
 	{
+	}
+
+	@Override
+	public boolean onKeyDown( int keyCode, KeyEvent event )
+	{
+		Log.i( "onKeyDown", "keyCode=" + keyCode );
+		if ( keyCode == KeyEvent.KEYCODE_MEDIA_NEXT || keyCode ==KeyEvent.KEYCODE_MEDIA_PREVIOUS 
+				|| keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+		{
+			if ( !this.started )
+			{
+				this.onReset( this.btnReset );
+				return true;
+			}
+			return false;
+		}
+		else if ( keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE  || keyCode == KeyEvent.KEYCODE_HEADSETHOOK )
+		{
+			this.onStartStop( this.btnStart );
+			return true;
+		}
+		return super.onKeyDown( keyCode, event );
 	}
 }
