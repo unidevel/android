af2a057c202f9cf6f29ee8276eeb7863d308e605
unidevel <unidevel@hotmail.com>
2015-04-06 22:39:21 +0800

    j

---
+++ ALibTest/src/com/example/alibtest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.alibtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.unidevel.widget.AppListView;
+
+public class MainActivity extends Activity {
+
+	AppListView listView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		this.listView = (AppListView) this.findViewById(R.id.listView);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
